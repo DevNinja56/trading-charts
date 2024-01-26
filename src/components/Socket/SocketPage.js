09be5c3f4c4eb15ac06b5129085236194bb02836
DevNinja56 <97098423+DevNinja56@users.noreply.github.com>
2024-01-26 23:25:06 +0500

    Added CSS className Reference

--- src/components/Socket/SocketPage.js
+++ src/components/Socket/SocketPage.js
@@ -3,14 +3,14 @@ import OrderBook from "./OrderBook";
 
 const SocketPage = () => {
   return (
-    <div style={{ textAlign: "center", padding: '10px', margin: '20px' }}>
+    <div className='page-container'>
             <h1 style={{ color: 'purple' }}> Socket Based Implementation </h1>
             <p>On this page we are getting live data for Candle Chart and Order Book using Bitfinex Websocket.</p> 
             <p>I used this approach because Websockets are always preferred in those scenarios where live data is required.</p>
             <a href="wss://api.bitfinex.com/ws/2" target="_blank" rel="noreferrer">Data source for Trade Chart and Order Book</a>
             <br />
             <a href="https://docs.bitfinex.com/reference/ws-public-trades" target="_blank" rel="noreferrer">Websocket Documentation</a>
-            <div style={{background: 'hsl(210,8%,85%)' }}>
+            <div className='candle-chart-container'>
                 <CandlestickChart></CandlestickChart>
             </div>
             <br /><br />
