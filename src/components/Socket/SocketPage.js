40f01e7b4f2557d605c84392413eb7f0c07fe57c
DevNinja56 <97098423+DevNinja56@users.noreply.github.com>
2024-01-26 19:13:48 +0500

    Updated socket page heading

--- src/components/Socket/SocketPage.js
+++ src/components/Socket/SocketPage.js
@@ -4,7 +4,7 @@ import OrderBook from "./OrderBook";
 const SocketPage = () => {
   return (
     <div style={{ textAlign: "center", padding: '10px', margin: '20px' }}>
-            <h1 style={{ color: 'purple' }}> API Based Implementation </h1>
+            <h1 style={{ color: 'purple' }}> Socket Based Implementation </h1>
             <p>On this page we are getting live data for Candle Chart and Order Book using Bitfinex Websocket.</p> 
             <p>I used this approach because Websockets are always preferred in those scenarios where live data is required.</p>
             <a href="wss://api.bitfinex.com/ws/2" target="_blank" rel="noreferrer">Data source for Trade Chart and Order Book</a>
