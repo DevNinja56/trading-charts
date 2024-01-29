98299202398b77c934e4cf73c8ebd33d7b90b566
DevNinja56 <shahzaib@Shahzaibs-MacBook-Pro.local>
2024-01-29 20:29:42 +0500

    Fixing values for ZRX and XRP pairs

--- src/components/API/ChartWithOption.js
+++ src/components/API/ChartWithOption.js
@@ -39,8 +39,8 @@ const ChartWithOptions = () => {
                     <select className='custom-select' value={selectedPair} onChange={handlePairChange}>
                         <option value="BTC/USD">BTC/USD</option>
                         <option value="ETH/USD">ETH/BTC</option>
-                        <option value="BTC/USD">ZRX/USD</option>
-                        <option value="ETH/USD">XRP/USD</option>
+                        <option value="ZRX/USD">ZRX/USD</option>
+                        <option value="XRP/USD">XRP/USD</option>
                     </select>
                 </div>
             </div>
--- src/components/Socket/OrderBookWithOptions.js
+++ src/components/Socket/OrderBookWithOptions.js
@@ -15,8 +15,8 @@ const OrderBookWithOptions = () => {
                     <select className='custom-select' value={selectedPair} onChange={handlePairChange}>
                         <option value="BTC/USD">BTC/USD</option>
                         <option value="ETH/USD">ETH/BTC</option>
-                        <option value="BTC/USD">ZRX/USD</option>
-                        <option value="ETH/USD">XRP/USD</option>
+                        <option value="ZRX/USD">ZRX/USD</option>
+                        <option value="XRP/USD">XRP/USD</option>
                     </select>
                 </div>
             <OrderBook selectedPair={selectedPair}/>
